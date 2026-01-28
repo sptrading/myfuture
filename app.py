b63c3ce318d8fec05da3ab92c0a5fdafac3350a9
sptrading <sptrading300123@gmail.com>
2026-01-28 16:49:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from fastapi import FastAPI
-from routes.api import router as api_router
-from routes.home import router as home_router
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+from routes.stocks import router as stocks_router
 
 app = FastAPI()
 
-app.include_router(home_router)
-app.include_router(api_router)
+# Templates setup
+templates = Jinja2Templates(directory="templates")
+
+# Routers
+app.include_router(stocks_router)
+
+
+# Home page
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
