ebd0cbc8b25ba8159a6d798ae03579b908a930e6
sptrading <sptrading300123@gmail.com>
2026-01-28 22:02:58 +0530

    Update database.py

--- services/database.py
+++ services/database.py
@@ -1,26 +1,25 @@
 import sqlite3
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DB_PATH = os.path.join(BASE_DIR, "stocks.db")
+DB_PATH = "/opt/render/project/src/stocks.db"
 
 def get_connection():
-    return sqlite3.connect(DB_PATH)
-
+    return sqlite3.connect(DB_PATH, check_same_thread=False)
 
 def init_db():
     conn = get_connection()
     c = conn.cursor()
 
-    c.execute('''
+    c.execute("""
         CREATE TABLE IF NOT EXISTS stock_data (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
             symbol TEXT,
             ltp REAL,
             prev_close REAL,
             change_percent REAL,
             timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
         )
-    ''')
+    """)
 
     conn.commit()
     conn.close()
