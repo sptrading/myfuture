7786c6a4ccf1f5e0db9e4d6a8b3f89c87f8878b3
sptrading <sptrading300123@gmail.com>
2026-01-28 18:04:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,11 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-import services.fetch_store  # 👈 THIS IS THE KEY
+import services.fetch_store  # 👈 MUST
 
 from routes.stocks import router as stocks_router
 
 app = FastAPI()
-
 templates = Jinja2Templates(directory="templates")
 
 app.include_router(stocks_router)
