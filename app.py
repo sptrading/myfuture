781517100a9fc566b30929a299a1645d5cedb2f9
sptrading <sptrading300123@gmail.com>
2026-01-29 00:10:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from services.stocks import router as stocks_router
-from services.fetch_store import start_background_fetch
+from routes.stocks import router as stocks_router
+from services.fetch_store import start_fetch_loop
+import threading
 
 app = FastAPI()
 
@@ -12,4 +13,4 @@ def root():
 
 @app.on_event("startup")
 def startup_event():
-    start_background_fetch()
+    threading.Thread(target=start_fetch_loop, daemon=True).start()
