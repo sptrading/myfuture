737c522eeb95e6ade3058a0d75d6d91751542f53
sptrading <sptrading300123@gmail.com>
2026-01-28 18:06:03 +0530

    Update database.py

--- services/database.py
+++ services/database.py
@@ -1,7 +1,15 @@
 import sqlite3
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DB_PATH = os.path.join(BASE_DIR, "stocks.db")
+
+def get_connection():
+    return sqlite3.connect(DB_PATH)
+
 
 def init_db():
-    conn = sqlite3.connect("stocks.db")
+    conn = get_connection()
     c = conn.cursor()
 
     c.execute('''
