f30faa99f67f874c46b0afa26358a3b4d9def4e9
sptrading <sptrading300123@gmail.com>
2026-01-28 18:00:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,22 +2,16 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+import services.fetch_store  # 👈 THIS IS THE KEY
+
 from routes.stocks import router as stocks_router
 
 app = FastAPI()
 
-# Templates
 templates = Jinja2Templates(directory="templates")
 
-# Routers
 app.include_router(stocks_router)
 
-
-# Start background Upstox fetch when app starts
-
-
-
-# Home page
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
