6eb258269ca58cdbcbd404698e1cbed2d19a5d80
sptrading <sptrading300123@gmail.com>
2026-01-29 14:19:36 +0530

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,8 +1,6 @@
-# runner.py
 import time
 from services.fetch_store import fetch_and_store
 
-# तुझ्या instruments list इथे टाक (200+)
 instrument_keys = [
     "NSE_EQ|INE466L01038",
     "NSE_EQ|INE117A01022",
