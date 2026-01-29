6bf209d4bae0ae2298d6c918074331db7a8d947c
sptrading <sptrading300123@gmail.com>
2026-01-29 12:35:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
+# app.py
 from fastapi import FastAPI
 from routes.stocks import router as stocks_router
-from services.fetch_store import start_background_fetch
 
 app = FastAPI()
 
-# Include routes
 app.include_router(stocks_router)
-
-@app.on_event("startup")
-def startup_event():
-    start_background_fetch()
-
 
 @app.get("/")
 def home():
-    return {"status": "ok"}
+    return {"message": "Upstox Scanner Running"}
