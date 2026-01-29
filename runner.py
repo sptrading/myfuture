c09d4b3efa2bd475e17bcdda723f56cca7a35301
sptrading <sptrading300123@gmail.com>
2026-01-29 12:51:35 +0530

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,14 +1,18 @@
 # runner.py
 import time
-from fetch_store import fetch_and_store
+from services.fetch_store import fetch_and_store
 
-# तुझ्या instruments.json मधून घे
+# तुझ्या instruments list इथे टाक (200+)
 instrument_keys = [
     "NSE_EQ|INE466L01038",
     "NSE_EQ|INE117A01022",
-    # बाकी तुझे 200+ keys इथे
+    # बाकी तुझे keys
 ]
 
 while True:
-    fetch_and_store(instrument_keys)
-    time.sleep(30)
+    try:
+        fetch_and_store(instrument_keys)
+        time.sleep(30)
+    except Exception as e:
+        print("Runner Error:", e)
+        time.sleep(10)
