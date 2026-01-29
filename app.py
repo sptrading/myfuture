cbe1fbdb950b9e3e93cb5007d6e0c87c4327c620
sptrading <sptrading300123@gmail.com>
2026-01-29 08:01:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,16 @@
 from fastapi import FastAPI
-import os
-
+from routes.stocks import router as stocks_router
 from services.fetch_store import start_background_fetch
-from services.stocks import router as stocks_router
 
 app = FastAPI()
 
 app.include_router(stocks_router)
 
-
-@app.get("/")
-def home():
-    return {"status": "ok"}
-
-
-# 🔥 VERY IMPORTANT — run only once
 @app.on_event("startup")
 def startup_event():
     start_background_fetch()
+
+
+@app.get("/")
+def root():
+    return {"status": "ok"}
